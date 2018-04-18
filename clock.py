e5f7ad93e0c356b7397b17956548c5038dbc3a98
hzg0601 <hzg0601@163.com>
2018-04-18 10:30:13 +0800

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from turtle import *
+from datetime import *
+
+def SetupClock(radius):
+    reset()
+    pensize(7)
+    for i in range(60):
+        Skip(radius)
+        if i%5==0:
+            forward(20)
+            Skip(-radius-20)
+        else:
+            dot(5)
+            Skip(-radius)
+        right(6)
+def Skip(step):
+    penup()
+    forward(step)
+    pendown()
+    
+def mkHand(name,length):
+    reset()
+    Skip(-length*0.1)
+    begin_poly()
+    forward(length*1.1)
+    end_poly()
+    handForm=get_poly()
+    register_shape(name,handForm)
+    
+def Init():
+    global secHand,minHand,hurHand,printer
+    mode('logo')
+    mkHand('secHand',125)
+    mkHand('minHand',130)
+    mkHand('hurHand',90)
+    secHand=Turtle()
+    secHand.shape('secHand')
+    minHand=Turtle()
+    minHand.shape('minHand')
+    hurHand=Turtle()
+    hurHand.shape('hurHand')
+    for hand in secHand,minHand,hurHand:
+        hand.shapesize(1,1,3)
+        hand.speed(0)
+    printer=Turle()
+    printer.hidetutle()
+    printer.penup()
+    
+def Tick():
+    t=datetime.today()
+    second=t.second+t.microsecond*0.000001
+    minute=t.minute+second/60.0
+    hour=t.hour+minute/60.0 
+    
+    tracer(False)
+    printer.forward(65)
+    printer.write(Week(t),align='center',font=("Courier",14,'bold'))
+    printer.back(130)
+    printer.write(Date(t),align='center',font=('Courier',14,'bold'))
+    printer.home()
+    tracer(True)
+    secHand.setheading(6*second)
+    minHand.setheading(6*minute)
+    hurHand.setheading(30*hour)
+    ontimer(Tick,100)#100ms后继续定义tick
+
+def main():
+    tracer(False)
+    Init()
+    SetupClock(160)
+    tracer(True)
+    Tick()
+    mainloop()
+
+
